3d0803b219aa9d21471ba3fcf7ffd204dfb0c1a0
abdoslg <abdoismen507@gmail.com>
2022-10-02 02:55:08 +0200

    add secure in http

--- main.js
+++ main.js
@@ -7,23 +7,24 @@ let   select=document.querySelector("select")
 
 
 btn.onclick=function(){
-   // fetch(`http://worldtimeapi.org/api/timezone/${select.value}`)
-   // .then((re)=>{
-   //    return re.json()
-   // })
-   // .then((re)=>{
-   //    result.innerHTML=`date is ${re.datetime.slice(0 , 10)} & time is ${re.datetime.slice(11 , -13)} in ${select.value}`
-   // })
-   // //*********************************** */
+   fetch(`https://worldtimeapi.org/api/timezone/${select.value}`)
+   .then((re)=>{
+      return re.json()
+   })
+   .then((re)=>{
+      result.innerHTML=`date is ${re.datetime.slice(0 , 10)} & time is ${re.datetime.slice(11 , -13)} in ${select.value}`
+   })
 
-   let myRequest = new XMLHttpRequest () ;
+   // //*********************************************************************** */
 
-   myRequest.onreadystatechange = function() {
-      if ( this.readyState === 4 && this.status === 200 ) {
-         result.innerHTML=`date is ${JSON.parse(this.responseText).datetime.slice(0 , 10)} & time is ${JSON.parse(this.responseText).datetime.slice(11 , -13)} in ${select.value}`
-      }
-   } 
-   myRequest.open("GET",`https://worldtimeapi.org/api/timezone/${select.value}` ) 
-   myRequest.send();
+   // let myRequest = new XMLHttpRequest () ;
+
+   // myRequest.onreadystatechange = function() {
+   //    if ( this.readyState === 4 && this.status === 200 ) {
+   //       result.innerHTML=`date is ${JSON.parse(this.responseText).datetime.slice(0 , 10)} & time is ${JSON.parse(this.responseText).datetime.slice(11 , -13)} in ${select.value}`
+   //    }
+   // } 
+   // myRequest.open("GET",`https://worldtimeapi.org/api/timezone/${select.value}` ) 
+   // myRequest.send();
 }
 
